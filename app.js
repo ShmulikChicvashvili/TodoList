6a487ebcf25599aa21665b2916ab9c95e44da146
Shmulik <s.chicvashvili@gmail.com>
2016-03-25 03:54:39 +0300

    Fixed dependencies issues.

--- app.js
+++ app.js
@@ -1,63 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var express  = require('express');
+var app      = express();                        // create our app w/ express
+var mongoose = require('mongoose');              // mongoose for mongodb
+var morgan   = require('morgan');                // log requests to the console (express4)
+var bodyParser = require('body-parser');         // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var database = require('./config/database_config');
+var port     = process.env.PORT || 8888;         // set the port
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// configuration ===============================================================
+mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
 
-var app = express();
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// routes ======================================================================
+require('./app/routes.js')(app);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-require('./app/routes')(app);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+// listen (start app with node server.js) ======================================
+app.listen(port);
+console.log("App listening on port : " + port);
